c58c867a3a46c8aabef46efaccee6476b5d22300
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2014-09-16 15:35:57 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+var nome = document.getElementById('userName');
+  var genero = document.getElementById('userGender');
+  var aniversario = document.getElementById('userBirth');
+  
+    function salvarDados() {
+      window.localStorage.setItem('nome',nome.value);
+      window.localStorage.setItem('genero',genero.value);
+      window.localStorage.setItem('aniversario',aniversario.value);
+      document.location.href = "index.html";
+    }
+    
+    function apagarDados() {
+      window.localStorage.clear();
+      document.location.href = "index.html";
+    }
+    
+function config() {
+  d.getElementById('config').style.display = 'block';
+}
+function closeConfig() {
+  d.getElementById('config').style.display = 'none';
+}
