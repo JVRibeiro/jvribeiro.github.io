2fd114b35733485816eda3a0bfe3c98247e4536e
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2014-10-08 02:08:35 -0300

    Update miley.config.js

--- miley.config.js
+++ miley.config.js
@@ -1,18 +1,17 @@
 var d = document;
-var w = window;
 nome = d.getElementById('userName'); 
 genero = d.getElementById('gen'); 
 
 function salvarDados() {
-	d.localStorage.setItem('nome',nome.value); 
-	d.localStorage.setItem('genero',genero.value); 
+	window.localStorage.setItem('nome',nome.value); 
+	window.localStorage.setItem('genero',genero.value); 
 	setTimeout(d.location.href = "", 2000);
 } 
 
 function apagarDados() {
 	confirmar = confirm("Use esse botão com cuidado!\n\n Seus dados não poderão ser recuperados.\n\n Você tem certeza que deseja apagar seus dados?"); 
 	if (confirmar == true) {
-		d.localStorage.clear(); 
+		window.localStorage.clear(); 
 		alert("Seus dados foram apagados. Aperte Ok para atualizar."); 
 		setTimeout(d.location.href = "", 2000);
 	} 
@@ -25,19 +24,19 @@ function apagarDados() {
 dialog = d.getElementById('dialogo'); 
 
 function getHist() {
-	d.localStorage.getItem('dialog');
+	window.localStorage.getItem('dialog');
 } 
 
 function saveHist() {
-	d.localStorage.setItem('histórico',dialog.value);
+	window.localStorage.setItem('histórico',dialog.value);
 } 
 
 function clearHist() {
-	d.localStorage.removeItem('histórico'); 
+	window.localStorage.removeItem('histórico'); 
 	setTimeout(d.location.href = "", 2000);
 } 
 
 function clearNome() {
-	d.localStorage.removeItem('nome'); 
+	window.localStorage.removeItem('nome'); 
 	setTimeout(d.location.href = "", 2000);
 };
