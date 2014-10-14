b915f545eedcf6cd1ff64141b495fe42e1f79c92
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2014-10-14 01:09:17 -0300

    01:09 14/10/2014

--- miley.comandos.js
+++ miley.comandos.js
@@ -63,12 +63,25 @@ var g1 = function(algo) {abrirWindowG1 = w.open('http://g1.globo.com/', 'g1', 'w
  
     $.ajax({
         type: "GET",
-        url: "https://pt.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+algo+"&redirects=&callback=?",
+        url: "https://pt.wikipedia.org/w/api.php?action=parse&format=json&exchars=450&exlimit=10&exintro=&explaintext=&exsectionformat=plain&prop=text&section=0&page="+algo+"&redirects=&callback=?",
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
             console.log(data);
+
+            var markup = data.parse.text["*"];
+            var blurb = $('<div></div>').html(markup);
+ 
+            // remove links as they will not work
+            blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+ 
+            // remove any references
+            blurb.find('sup').remove();
+ 
+            // remove cite error
+            blurb.find('.mw-ext-cite-error').remove();
+            $('#resposta').val($(blurb).find('p'));
         },
         error: function (errorMessage) {
         }
