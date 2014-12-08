ec0922d45bd8194ce93dc3e716546337ba6a6f59
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2014-12-08 01:25:08 -0200

    Correções de bug

--- js/miley.brain.js
+++ js/miley.brain.js
@@ -335,8 +335,57 @@ window.fbAsyncInit = function() {
 					oauth      : true                                  // Enable oauth authentication
 				});
 
+
+        // Check if the current user is logged in and has authorized the app
+      FB.getLoginStatus(checkLoginStatus);
+
+      // Login in the current user via Facebook and ask for email permission
+      function authUser() {
+        FB.login(checkLoginStatus, {scope:'email, publish_stream'});
+      }
+
+      // Check the result of the user status and display login button if necessary
+      function checkLoginStatus(response) {
+        if(response && response.status == 'connected') {
+          alert('User is authorized');
+
+          // Hide the login button
+          document.getElementById('fb-login-buttom').style.display = 'none';
+
+          // Now Personalize the User Experience
+          console.log('Access Token: ' + response.authResponse.accessToken);
+        } else {
+          alert('User is not authorized');
+
+          // Display the login button
+          document.getElementById('fb-login-buttom').style.display = 'block';
+        }
+      }
+
+
+
+
 				// Additional initialization code such as adding Event Listeners goes here
 function post_on_wall() {
+
+  FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    // the user is logged in and has authenticated your
+    // app, and response.authResponse supplies
+    // the user's ID, a valid access token, a signed
+    // request, and the time the access token
+    // and signed request each expire
+    var uid = response.authResponse.userID;
+    var accessToken = response.authResponse.accessToken;
+  } else if (response.status === 'not_authorized') {
+    // the user is logged in to Facebook,
+    // but has not authenticated your app
+  } else {
+    // the user isn't logged in to Facebook.
+  }
+ });
+
+
 				FB.login(function(response)
 				{
 					if (response.authResponse)
