57f90b3d5c95272a67cd5e9d94facd15cd2c3a29
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2014-12-07 23:30:26 -0200

    Correções de bug

--- js/miley.brain.js
+++ js/miley.brain.js
@@ -407,7 +407,7 @@ function post_on_wall() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/pt_BR/all.js";
+    js.src = "//connect.facebook.net/pt_BR/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
